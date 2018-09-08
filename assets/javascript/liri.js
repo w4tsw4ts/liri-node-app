95850119cc6b41c8a06f9cf1802191698a3a6fd4
w4tsw4ts <billy.montgomery@ge.com>
2018-09-08 18:46:44 -0400

    initial files v1

---
+++ assets/javascript/liri.js
@@ -0,0 +1,22 @@
+
+require("dotenv").config();
+
+
+var spotify = new Spotify(keys.spotify);
+
+switch (process.argv[2]) {
+    case concert-this:
+        //code block
+        break;
+    case spotify-this-song:
+        //code block
+        break;
+    case movie-this:
+        //code block
+        break;
+    //case do-what-it-says:
+        //code block
+        break;
+    default:
+        //code block
+}
