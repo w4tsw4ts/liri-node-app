8f2f80df03971f7db3204b2736e2599763996775
w4tsw4ts <billy.montgomery@ge.com>
2018-09-11 08:01:50 -0400

    v3 Added spofity-this-song

--- liri.js
+++ liri.js
@@ -1,65 +1,123 @@
+/* Make it so liri.js can take in one of the following commands:
+   concert-this
+   spotify-this-song
+   movie-this
+   do-what-it-says
+*/
 
-// Load .env key data
+// Load .env and key data
 require("dotenv").config();
-console.log(SPOTIFY_ID);
-console.log(SPOTIFY_SECRET);
-/*
+var keys = require("./keys.js");
+
+// required NPM Packages
+// npm require
+//var fs = require("fs");
+var request = require('request');
+var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 
+if (process.argv[2] == "--help" || process.argv[2] === "-h") {
+    console.log("usage: liri.js [-h]");
+    console.log(""); 
+    console.log("optional arguments:");
+    console.log("-h, --help         show this help message and exit");
+    console.log("");
+    console.log("node liri.js concert-this <artist/band name here>");
+    console.log("");
+    console.log("node liri.js spotify-this-song '<song name here>");
+    console.log("");
+    console.log("node liri.js movie-this '<movie name here>");
+    console.log("");
+    console.log("node liri.js do-what-it-says");
+    console.log("");
+    return;
+}
+
+// Main program
 switch (process.argv[2]) {
+    // node liri.js concert-this <artist/band name here>
     case "concert-this":
         concertThis();
         break;
+    // node liri.js spotify-this-song '<song name here>
     case "spotify-this-song":
-        spotifyThisSong();
+        spotifySong(process.argv[3]);
         break;
+    // node liri.js movie-this '<movie name here>
     case "movie-this":
-        movieThis();
+        movieThis(process.argv[3]);
         break;
+    // node liri.js do-what-it-says
     case "do-what-it-says":
         doWhatISay();
         break;
-}
+};
 
 function concertThis() {
+    console.log("concert=this function");
+    console.log(process.argv[3]);
+    artist = process.argv[3];
+    request("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp", function (error, response, body) {
 
-}
+        // If the request was successful...
+        if (!error && response.statusCode === 200) {
 
-function spotifyThisSong() {
-    requestAnimationFrame("https://api.spotify.com/v1/tracks/" + SPOTIFY_SECRET);
+            // Then log the body from the site!
+            console.log("statusCode: " + JSON.stringify(response.statusCode, null, 2));
+            console.log(JSON.body);
+        }
+    });
+};
 
-}
+function spotifySong(song) {
+    console.log("spotify-this-song")
+    console.log(song);
+    if (song === undefined){
+        song = "The Sign"
+    }
+    spotify.search({ type: 'track', query: song }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        //console.log(JSON.stringify(data, null, 2));
+        console.log("")
+        for(var i = 0; i < data.tracks.items.length; i++) {
+            console.log("Artist: " + data.tracks.items[i].album.artists[0].name);
+            console.log("Song: " + song);
+            console.log("Preview link: " + data.tracks.items[i].album.artists[0].external_urls.spotify);
+            console.log("Album name: " + data.tracks.items[i].album.name);
+            console.log("--------------------------------------------------------------------------------");
+        };
 
-function movieThis() {
+    });
+};
 
-}
+function movieThis(movieName) {
+    console.log("movie-this");
+    // Then run a request to the OMDB API with the movie specified
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+    // This line is just to help us debug against the actual URL.
+    console.log(queryUrl);
+
+    request(queryUrl, function (error, response, body) {
+
+        // If the request is successful
+        if (!error && response.statusCode === 200) {
+
+            // Parse the body of the site and recover just the imdbRating
+            // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+            console.log("Release Year: " + JSON.parse(body).Year);
+        }
+    });
+};
 
 function doWhatISay() {
-
-}
-*/
-
-/*
-// fs is a core Node package for reading and writing files
-var fs = require("fs");
-
-// This block of code will read from the "random.txt" file.
-// The code will store the contents of the reading inside the variable "spotifyData"
-fs.readFile("random.txt", "utf8", function(error, spotifyData) {
-
-  // If the code experiences any errors it will log the error to the console.
-  if (error) {
-    return console.log(error);
-  }
-
-  // We will then print the contents of spotifyData
-  console.log(spotifyData);
-
-  // Then split it by commas (to make it more readable)
-  var dataArr = spotifyData.split(",");
-
-  // We will then re-display the content as an array for later use.
-  console.log(dataArr);
-
-});
-*/
+    console.log("do-what-it-says");
+    spotify.search({ type: 'track', query: song }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        console.log(JSON.stringify(data, null, 2));
+    });
+};
